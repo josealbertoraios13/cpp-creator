9315dcdc66c7b9c8b8db0df81c904845521efbe9
albertoraios13 <albertofreconvick@gmail.com>
2026-01-27 20:27:37 -0300

    ADD-1<feature>: Script de instalação criado e testado

--- install.sh
+++ install.sh
@@ -1,3 +1,36 @@
-mkdir -p ~/cpp_creator
+set -e
 
-g
+
+mkdir -p ~/cpp_creator
+cp -r ./* ~/cpp_creator
+
+chmod +x ~/cpp_creator/alcpp.sh
+
+mkdir -p ~/.local/bin
+ln -sf ~/cpp_creator/alcpp.sh ~/.local/bin/alcpp
+
+case "$(basename "$SHELL")" in
+    zsh)  
+        RC="$HOME/.zshrc" 
+        ;;
+    bash) 
+        RC="$HOME/.bashrc" 
+        ;;
+    *)    
+        RC="$HOME/.profile" 
+        ;;
+esac
+
+echo "$RC"
+
+if ! grep -q "$HOME/.local/bin" "$RC"; then
+    echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$RC"
+fi
+
+if [ -e ~/.local/bin/alcpp ]; then
+    echo -e "*****\nAfter installation, you can delete this folder."
+else
+    echo "Erro"
+fi
+
+which alcpp
