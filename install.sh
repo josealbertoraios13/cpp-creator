02ae2cbaed39030f712dd4db3f70c42821907001
albertoraios13 <albertofreconvick@gmail.com>
2026-01-30 13:01:24 -0300

    hotfix(install.sh): script de instalação teve bugs corrigidos

--- install.sh
+++ install.sh
@@ -2,38 +2,25 @@
 set -e
 
 # creates a directory where will be installed this program
-mkdir -p "$HOME/cpp_creator"
-cp -r ./* "$HOME/cpp_creator"
-
-# Get the exact path to this script and store it in a variable.
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
-# Go to home directory
-cd "$HOME"
-
-# Delete the installation directory.
-rm -rf $SCRIPT_DIR
+mkdir -pv "$HOME/cpp_creator"
+cp -rv ./* "$HOME/cpp_creator"
 
 # Allows this file to be executed.
-chmod +x "cpp_creator/alcpp.sh"
+chmod +x "$HOME/cpp_creator/alcpp.sh"
 
-mkdir -p ".local/bin"
+mkdir -pv "$HOME/.local/bin"
 
 # Create a symbolic link so 'alcpp' can be run as a global command
 # -s: symbolic, -f: force overwrite if exists
-ln -sf "cpp_creator/alcpp.sh" ".local/bin/alcpp"
+ln -sfv "$HOME/cpp_creator/alcpp.sh" "$HOME/.local/bin/alcpp"
 
 # Detect the active shell to determine the correct configuration file
 case "$(basename "$SHELL")" in
     zsh)  
-        RC=".zshrc" 
+        RC="$HOME/.zshrc" 
         ;;
     bash) 
-        RC=".bashrc" 
-        ;;
-    fish)
-        # Fish shell uses a specific directory structure for its configuration.
-        RC=".config/fish/config.fish"
+        RC="$HOME.bashrc" 
         ;;
     *)
         # Generic fallback for other shells (sh, dash, etc.)    
@@ -44,13 +31,23 @@ esac
 # Check if ".local/bin" is already in the config file.
 # grep -q: quiet mode (returns exit status without printing output).
 # !: logical NOT (proceeds if the string is NOT found).
-if ! grep -q ".local/bin" "$RC"; then
+if ! grep -qv "$HOME/.local/bin" "$RC"; then
     echo 'export PATH="$HOME/.local/bin:$PATH"' >> "$RC"
 fi
 
 # Check if the installation was successful.
-if [ -e ".local/bin/alcpp" ]; then
+if [ -e "$HOME/.local/bin/alcpp" ]; then
     echo -e "Installation completed successfully. \nUse alcpp --help to view the available commands."
 else
     echo "The installation failed."
+    exit 1
 fi
+
+# Get the exact path to this script and store it in a variable.
+#SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+# Go to home directory
+#cd "$HOME"
+
+# Delete the installation directory.
+#rm -rf $SCRIPT_DIR
